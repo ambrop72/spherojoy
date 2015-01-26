90062b932109b6ec6f0dc6a4096f02840714cb63
Ambroz Bizjak <ambrop7@gmail.com>
2015-01-26 21:11:55 +0100

    Accept and require configuration in a config file. We also no longer support running directly from source.

--- spherojoy.nix
+++ spherojoy.nix
@@ -1,32 +1,24 @@
-{ stdenv, writeText, writeScriptBin, coreutils, python27, bash, badvpn }:
+{ stdenv, writeScriptBin, coreutils, python27, bash, badvpn }:
 let
     spherojoySource = stdenv.lib.cleanSource ./.;
     
-    mainNcdScript = writeText "spherojoy.ncd" ''
-        include "${spherojoySource}/spheroncd.ncdi"
+    fixedSource = stdenv.mkDerivation {
+        name = "spherojoy-fixed-source";
         
-        process main {
-            If (@true) {
-                var("/dev/input/by-id/usb-Logitech_Logitech_Extreme_3D-event-joystick") joy_dev;
-                value(["X": "0",    "Y": "0",    "RZ": "0",   "THROTTLE": "255"]) joy_axis_min;
-                value(["X": "1023", "Y": "1023", "RZ": "255", "THROTTLE": "0"  ]) joy_axis_max;
-                var("60") rz_degrees;
-                var("/dev/rfcomm0") sphero_dev;
-                var("5") max_sendq_len;
-                var("20") max_roll_interval;
-                var(@false) print_joy_events;
-            } config;
-            
-            var("${coreutils}/bin/stty") stty;
-            var("${python27}/bin/python") python;
-            var("${spherojoySource}") spherojoy_dir;
-            
-            call(@_spheroncd, {^config, stty, python, spherojoy_dir});
-        }
-    '';
+        src = spherojoySource;
+        
+        installPhase = ''
+            substituteInPlace main.ncd \
+                --replace /STTY/ "${coreutils}/bin/stty" \
+                --replace /PYTHON27/ "${python27}/bin/python" \
+                --replace /SOURCE_DIR/ "$out"
+            mkdir -p "$out"
+            cp -r ./* "$out"/
+        '';
+    };
 
 in
 writeScriptBin "spherojoy" ''
     #!${bash}/bin/bash
-    exec ${badvpn}/bin/badvpn-ncd --loglevel notice ${mainNcdScript} "$@"
+    exec ${badvpn}/bin/badvpn-ncd --loglevel notice "${fixedSource}/main.ncd" "$@"
 ''
